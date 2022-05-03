f7d4a73d51b1a51d6dff37f2a55b319153da223f
amine <amine.selmi@esprit.tn>
2022-05-03 22:26:42 +0100

    service event

--- Frontend/src/app/events/events.component.ts
+++ Frontend/src/app/events/events.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {HttpClientModule} from '@angular/common/http'
+import { FormBuilder } from '@angular/forms';
+import {EventsService} from '../services/events.service'
 
 @Component({
   selector: 'app-events',
@@ -6,10 +9,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./events.component.css']
 })
 export class EventsComponent implements OnInit {
-  name = "amine"
-  constructor() { }
+  
+  constructor(private formBuilder: FormBuilder,
+    private eventService :EventsService) { }
 
   ngOnInit(): void {
   }
-
+  
+  addEventForm = this.formBuilder.group({
+    title: '',
+    description: '',
+    type:'',
+    date: '',
+    image: '',
+  });
+  onSubmit(){
+    let event = this.addEventForm.value
+    this.eventService.addEvent(event)
+    .subscribe(()=>console.log('event added')
+    )
+  }
 }
