1da52185620c4b04e652bd6dfc1547384f6d2e1c
Youssef-Bedoui <Jozefbedoui@gmail.com>
2022-05-05 00:45:06 +0100

    Revert "add event finished"

--- Backend/index.js
+++ Backend/index.js
@@ -12,12 +12,10 @@ const PORT = process.env.PORT || 3000
 const cors = require('cors');
 
 app.use(cors())
-app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
-app.use(bodyParser.json({limit: '50mb',limit: '50mb'}));
-app.use(express.json({limit: "50mb"}));
-app.use(express.urlencoded({limit: '50mb', extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(__dirname + "/../client/public"));
-
+app.use(bodyParser.json({limit: '50mb'}));
 app.use(fileUpload());
 
 
--- Frontend/src/app/events/events.component.ts
+++ Frontend/src/app/events/events.component.ts
@@ -1,7 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import {HttpClient} from '@angular/common/http'
-import { FormBuilder } from '@angular/forms';
-import {EventsService} from '../services/events.service'
 
 @Component({
   selector: 'app-events',
@@ -9,64 +6,10 @@ import {EventsService} from '../services/events.service'
   styleUrls: ['./events.component.css']
 })
 export class EventsComponent implements OnInit {
-  
-   url:any = ''
-   type:string=''
-  constructor(private formBuilder: FormBuilder,
-    private eventService :EventsService,
-    private http: HttpClient) { }
+  name = "amine"
+  constructor() { }
 
   ngOnInit(): void {
   }
-   
-  addEventForm = this.formBuilder.group({
-    title: '',
-    description: '',
-    date: '',
-  });
-  onSubmit(){
-    
-    let event = this.addEventForm.value
-    event.image = this.url
-    event.type = this.type
-    console.log(event,'event')
-    this.eventService.addEvent(event)
-    .subscribe(()=>console.log('event added')
-    )
-  }
-  handleTypeEvent(event:any){
-    this.type = event.target.value
-  }
-  onFileChange(event){
-    let reader = new FileReader();
-    reader.readAsDataURL(event.target.files[0]);
-    reader.onload = () => {
-      this.url = reader.result;
-     
-    };
-  }
-  // onFileChange(event) {
-  //   var files = event.target.files[0];
-  //   console.log(files,'image from form')
-  //    if (files) {
-      
 
-  //     const formData = new FormData();
-
-  //     formData.append("thumbnail", files);
-
-  //     const upload$ = this.http.post("http://localhost:3000/api/event/add", formData);
-
-  //     upload$.subscribe();
-  //    }
-  //   //  this.createImage(files[0]);
-  // }
-  // // createImage(file) {
-  // //   var reader = new FileReader();
-  // //   var vm = this;
-  // //   reader.onload = (e) => {
-  // //     vm.image = e.target.result;
-  // //   };
-  // //   reader.readAsDataURL(file);
-  // // }
 }
