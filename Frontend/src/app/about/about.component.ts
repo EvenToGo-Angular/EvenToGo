30b6fa5b9ddd76eb53163694a26e99f37a17ad5c
ALOU19 <alaa.oueslati@esprit.tn>
2022-05-04 09:27:12 +0100

    about component

--- Frontend/src/app/about/about.component.ts
+++ Frontend/src/app/about/about.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css']
 })
-export class AboutComponent implements OnInit {
+export class AboutComponent  {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+  constructor(@Inject(DOCUMENT) private document:Document) { }
+  
+  
+  
+   goToUrl1(): void {
+   this.document.location.href = 'https://github.com/ALOU19'}
+    goToUrl12(): void {
+     this.document.location.href = 'https://github.com/amine-selmi'}
+      goToUrl3(): void {
+       this.document.location.href = 'https://github.com/Youssef-Bedoui'}
+        goToUrl4(): void {
+         this.document.location.href = 'https://github.com/imedrahal'}
+          goToUrl5(): void {
+           this.document.location.href = 'https://github.com/SlimGuenidez'}
 
 }
